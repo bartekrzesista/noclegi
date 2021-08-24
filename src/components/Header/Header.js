3d2fc8fe9d4e6de41fd2c856a1e15423cc25c00a
Bartłomiej Rzęsista <bartek.rzesista@gmail.com>
2021-08-24 17:46:36 +0200

    add parallax for header background

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,14 +3,14 @@ import styles from './Header.module.css';
 import withMousePosition from '../../hoc/withMousePosition';
 
 function Header(props) {
+    const parallax = {
+        transform: `translate(${props.mouseX/60}px, ${props.mouseY/120}px)`
+    }
+
     return (
         <header className={styles.header}>
+            <div className={styles.headerBackground} style={parallax}></div>
             <h1 className={styles.heading}>Noclegi</h1>
-            <div className="text-light">
-                {props.mouseX}
-                <br />
-                {props.mouseY}
-            </div>
             {props.children}
         </header>
     )
