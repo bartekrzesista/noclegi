08e2cdffd67310fa28a1d88aaf31b31cd1ca081b
Bartłomiej Rzęsista <bartek.rzesista@gmail.com>
2021-08-27 21:38:31 +0200

    optimize Hotels component (class)

--- src/components/Hotels/Hotels.js
+++ src/components/Hotels/Hotels.js
@@ -1,10 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Component } from "react";
+import { PureComponent } from "react";
 import Hotel from "./Hotel/Hotel";
 import styles from "./Hotels.module.css";
 
-class Hotels extends Component {
+class Hotels extends PureComponent {
+  componentDidUpdate() {
+    console.log('Hotels rendered');
+  }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   return this.props.hotels === nextProps.hotels ? false : true;
+  // }
+
   render() {
     return (
       <div className={styles.container}>
