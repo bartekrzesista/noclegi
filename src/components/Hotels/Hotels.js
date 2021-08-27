8611bb66a3bc5f729e361c41bc91669dbec72678
Bartłomiej Rzęsista <bartek.rzesista@gmail.com>
2021-08-27 22:51:37 +0200

    rewrite Hotels for functional component and optimize it

--- src/components/Hotels/Hotels.js
+++ src/components/Hotels/Hotels.js
@@ -1,32 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { PureComponent } from "react";
 import Hotel from "./Hotel/Hotel";
 import styles from "./Hotels.module.css";
 
-class Hotels extends PureComponent {
-  componentDidUpdate() {
-    console.log('Hotels rendered');
-  }
-
-  // shouldComponentUpdate(nextProps, nextState) {
-  //   return this.props.hotels === nextProps.hotels ? false : true;
-  // }
-
-  render() {
-    return (
-      <div className={styles.container}>
-        <h2 className={styles.title}>Oferty:</h2>
-        {this.props.hotels.map((e) => (
-          <Hotel key={e.id} hotel={e} />
-        ))}
-      </div>
-    );
-  }
+function Hotels(props) {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Oferty:</h2>
+      {props.hotels.map((e) => (
+        <Hotel key={e.id} hotel={e} />
+      ))}
+    </div>
+  );
 }
 
 Hotels.propTypes = {
   hotels: PropTypes.array.isRequired,
 };
 
-export default Hotels;
+const areEqual = (prevProps, nextProps) => {
+  return prevProps.hotels === nextProps.hotels;
+};
+
+export default React.memo(Hotels, areEqual);
