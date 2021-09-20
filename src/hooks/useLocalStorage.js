14e062e24dc3d0899c9ccab87c7f2421243abaf7
Bartłomiej Rzęsista <bartek.rzesista@gmail.com>
2021-09-20 16:38:09 +0200

    fix useState default value for useLocalStorage

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,15 +1,15 @@
 import { useState } from "react";
 
 export default function useLocalStorage(key, defaultValue) {
-    const [state, setState] = useState(defaultValue);
-
-    const storageValue = window.localStorage.getItem(key);
-    let value = storageValue ? JSON.parse(storageValue) : state;
+    const [state, setState] = useState(() => {
+        const storageValue = window.localStorage.getItem(key);
+        return storageValue ? JSON.parse(storageValue) : defaultValue;
+    });
 
     const setValue = val => {
         setState(val);
         window.localStorage.setItem(key, JSON.stringify(val));
     }
 
-    return [value, setValue];
+    return [state, setValue];
 }
