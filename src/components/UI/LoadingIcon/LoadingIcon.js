83d3fdf826a7209fa54b3622a20b2b40e7724a4e
Bartłomiej Rzęsista <bartek.rzesista@gmail.com>
2021-10-04 21:11:54 +0200

    add form handling for login page

--- src/components/UI/LoadingIcon/LoadingIcon.js
+++ src/components/UI/LoadingIcon/LoadingIcon.js
@@ -2,7 +2,7 @@ export default function LoadingIcon() {
   return (
     <div className="d-flex justify-content-center">
       <div className="spinner-border" role="status">
-        <span className="visually-hidden">Loading...</span>
+        <span className="visually-hidden">Ładowanie...</span>
       </div>
     </div>
   );
--- src/pages/Auth/LoginPage/LoginPage.js
+++ src/pages/Auth/LoginPage/LoginPage.js
@@ -1,15 +1,49 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import useAuth from "../../../hooks/useAuth";
+import LoadingButton from "../../../components/UI/LoadingButton/LoadingButton";
+
 export default function LoginPage() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [auth, setAuth] = useAuth();
+    const history = useHistory();
+
+    const submit = (e) => {
+        e.preventDefault();
+        setLoading(true);    
+
+        setTimeout(() => {
+            setAuth(true);
+            history.push("/");
+        }, 500);
+    };
+
     return (
         <div>
             <h2 className="mb-4">Logowanie</h2>
-            <form>
+            <form onSubmit={submit}>
                 <label htmlFor="loginEmail" className="form-label">Email:</label>
-                <input type="email" className="form-control mb-2" id="loginEmail" />
+                <input
+                    type="email"
+                    className="form-control mb-2"
+                    id="loginEmail"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                />
 
                 <label htmlFor="loginPassword" className="form-label">Hasło:</label>
-                <input type="password" className="form-control mb-3"    id="loginPassword" />
+                <input
+                    type="password"
+                    className="form-control mb-3"
+                    id="loginPassword"
+                    value={password}
+                    onChange={e => {setPassword(e.target.value)}}
+                />
 
-                <button className="btn btn-primary">Zaloguj</button>
+                <LoadingButton loading={loading} label="Zaloguj" />
+
             </form>
         </div>
     );
