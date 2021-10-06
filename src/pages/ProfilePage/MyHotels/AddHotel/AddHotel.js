78ee82f002cfd7c8060da66d267a2c8dfa2c743c
Bartłomiej Rzęsista <bartek.rzesista@gmail.com>
2021-10-06 22:28:17 +0200

    add styles for AddHotel form

--- src/pages/ProfilePage/MyHotels/AddHotel/AddHotel.js
+++ src/pages/ProfilePage/MyHotels/AddHotel/AddHotel.js
@@ -1,8 +1,11 @@
+import LoadingButton from "../../../../components/UI/LoadingButton/LoadingButton";
+
 const AddHotel = (props) => {
   return (
     <div className="card">
-      <div className="card-header">Nowy Hotel</div>
+      <h2 className="card-header">Nowy hotel</h2>
       <div className="card-body">
+        <p className="text-muted">Uzupełnij dane hotelu</p>
         <form>
           <div className="form-group">
             <label htmlFor="hotelName" className="form-label">
@@ -16,7 +19,7 @@ const AddHotel = (props) => {
             <div className="invalid-feedback">Błąd</div>
           </div>
 
-          <div className="form-group">
+          <div className="form-group mt-3">
             <label htmlFor="hotelCity" className="form-label">
               Miejscowość
             </label>
@@ -28,11 +31,11 @@ const AddHotel = (props) => {
             <div className="invalid-feedback">Błąd</div>
           </div>
 
-          <div className="form-group">
+          <div className="form-group mt-3">
             <label htmlFor="hotelRooms" className="form-label">
               Liczba pokoi
             </label>
-            <select className="form-control" name="rooms" id="hotelRooms">
+            <select className="form-select" name="rooms" id="hotelRooms">
               <option value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -40,32 +43,83 @@ const AddHotel = (props) => {
             </select>
           </div>
 
-          <div className="form-group">
-            <h3>Udogodnienia</h3>
-            <label>
-              TV <input type="checkbox" name="tv" />
-            </label>
-            <label>
-              Wi-Fi <input type="checkbox" name="wi-fi" />
-            </label>
-            <label>
-              Parking <input type="checkbox" name="parking" />
-            </label>
+          <div className="form-group mt-3">
+            <h4>Udogodnienia</h4>
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                name="tv"
+                id="tv"
+              />
+              <label className="form-check-label" htmlFor="tv">
+                TV
+              </label>
+            </div>
+
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                name="wi-fi"
+                id="wi-fi"
+              />
+              <label className="form-check-label" htmlFor="wi-fi">
+                Wi-Fi
+              </label>
+            </div>
+
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                name="parking"
+                id="parking"
+              />
+              <label className="form-check-label" htmlFor="parking">
+                Parking
+              </label>
+            </div>
           </div>
 
-          <div className="form-group">
-            <label htmlFor="hotelImg">Zdjęcie</label>
-            <input type="file" id="hotelImg" />
+          <div className="form-group mt-3">
+            <h4>Zdjęcie</h4>
+            <input type="file" id="hotelImg" aria-label="Hotel picture" />
           </div>
 
-          <div className="form-group">
-            <h3>Aktywny</h3>
-            <label>
-              Tak <input type="radio" name="active" />
-            </label>
-            <label>
-              Nie <input type="radio" name="active" />
-            </label>
+          <div className="form-group mt-3">
+            <h4>Status</h4>
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="status"
+                id="statusActive"
+              />
+              <label className="form-check-label" htmlFor="statusActive">
+                Aktywny
+              </label>
+            </div>
+
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="status"
+                id="statusHidden"
+              />
+              <label className="form-check-label" htmlFor="statusHidden">
+                Ukryty
+              </label>
+            </div>
+          </div>
+
+          <div className="d-flex justify-content-end mt-3">
+          <LoadingButton
+          loading={false}
+          className="btn-success">
+              Dodaj hotel
+            </LoadingButton>
           </div>
         </form>
       </div>
